6354f384ee92b4ab8beb464bea819c719fc4bc2c
meng <mengzhou44@gmail.com>
2023-09-17 10:10:45 -0600

    commit

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -6,7 +6,7 @@ import { TicketUpdatedListener } from './events/listeners/ticket-updated-listene
 import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener'
 
 const start = async () => {
-  console.log('starting ...')
+  console.log('starting 11111')
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined')
   }
